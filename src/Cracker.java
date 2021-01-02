80f81d01766dd214d312f6569c6303da5a7d7439
tkhadir <tkhadir.dev@gmail.com>
2021-01-02 13:09:21 +0100

    :rocket: add changes

--- src/Cracker.java
+++ src/Cracker.java
@@ -1,2 +1,63 @@
-package com.samples.tkhadir.src;public class Cracker {
+package com.samples.tkhadir.src;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+class Cracker implements Runnable {
+
+    private int start;
+    private int end;
+    private final MessageDigest digest = MessageDigest.getInstance("SHA-256");
+    private static boolean DONE = false;
+    private String found;
+
+    public Cracker(int s, int e) throws NoSuchAlgorithmException {
+        start = s;
+        end = e;
+    }
+
+    public void generate(StringBuilder sb, int n) {
+        if (DONE)
+            return;
+
+        if (n == sb.length()) {
+            String candidate = sb.toString();
+            // check password
+            byte[] bytes = digest.digest(candidate.getBytes());
+
+            if (Arrays.equals(bytes, PasswordCracker.BYTES_SHA_256_TO_FIND)) {
+                found = candidate;
+                DONE = true;
+            }
+
+            return;
+        }
+
+        for (int i = 0; i < PasswordCracker.ALPHABET.length && !DONE; i++) {
+            char letter = PasswordCracker.ALPHABET[i];
+            sb.setCharAt(n, letter);
+            generate(sb, n + 1);
+        }
+
+    }
+
+    @Override
+    public void run() {
+
+        for (int length = start; length <= end && !DONE; length++) {
+            StringBuilder sb = new StringBuilder();
+            sb.setLength(length);
+            generate(sb, 0);
+        }
+
+        if (DONE && found != null && !found.trim().isEmpty()) {
+            long duration = System.currentTimeMillis() - PasswordCracker.START_TIME;
+            System.out.println("Password cracked in " + TimeUnit.MILLISECONDS.toSeconds(duration) + "." + TimeUnit.MILLISECONDS.toMillis(duration) + " sec. Password was = " + found);
+        } else {
+            System.out.println("Password not cracked for subset [" + start + ", " + end + "]");
+        }
+    }
+
 }
--- src/PasswordCracker.java
+++ src/PasswordCracker.java
@@ -1,2 +1,51 @@
-package com.samples.tkhadir.src;public class PasswordCracker {
+package com.samples.tkhadir.src;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class PasswordCracker {
+
+    public static char[] ALPHABET = "abcdefghijklmnopqrstuvwxyz0123456789@#&!()^=+/:.;,".toCharArray();
+    public static final int PASSWORD_MAX_LENGTH = 30;
+    public static final String PASSWORD_SHA_256_TO_FIND = "8c9713d70976a2d1ea0c07a6d70998dc7da3ebca901f5b55cce470191bc86332";
+    public static final byte[] BYTES_SHA_256_TO_FIND = PasswordCracker.hexStringToByteArray(PASSWORD_SHA_256_TO_FIND);
+    public static long START_TIME;
+
+    public static byte[] hexStringToByteArray(String s) {
+        int len = s.length();
+        byte[] data = new byte[len / 2];
+        for (int i = 0; i < len; i += 2) {
+            data[i / 2] = (byte)((Character.digit(s.charAt(i), 16) << 4) +
+                    Character.digit(s.charAt(i + 1), 16));
+        }
+        return data;
+    }
+
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        int cores = Runtime.getRuntime().availableProcessors();
+        int lengthSet = PASSWORD_MAX_LENGTH / cores;
+        int end = 0;
+
+        START_TIME = System.currentTimeMillis();
+
+        ExecutorService executorService = Executors.newFixedThreadPool(cores);
+
+        while (end < PASSWORD_MAX_LENGTH) {
+            int start = end + 1;
+            end = start + lengthSet;
+
+            if (end > PASSWORD_MAX_LENGTH)
+                end = PASSWORD_MAX_LENGTH;
+
+            executorService.submit(new Cracker(start, end));
+        }
+
+        executorService.shutdown();
+
+    }
+
 }
